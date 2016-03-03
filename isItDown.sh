69392e3e38328b7f9635d14173d47bbcc21b870e
Chris <z.chris.92@gmail.com>
2016-03-03 14:30:32 +0100

    Create isItDown.sh

---
+++ isItDown.sh
@@ -0,0 +1,24 @@
+read -r -p "Please enter the server you want to monitor : " c
+
+echo "Pinging server for response " $c
+echo "Press [CTRL+C] to stop.."
+
+authentication=''
+if [[ $c =~ .*qa.* ]]
+then
+   	authentication=https://username:password
+   	c=$authentication$c
+  	echo "Using authentication for QA system: " $c
+else
+	headers=$'--header \'X-Forwarded-Proto: https\' --header \'Host: www.exmpleHost.com\''
+fi
+
+while :
+do
+	response=$(echo curl --write-out %{http_code} $headers --silent --output /dev/null $c | bash)
+	
+	if [ $response -ne 200 ]; then
+		echo "$(date)" $response
+	fi
+	sleep 1
+done
